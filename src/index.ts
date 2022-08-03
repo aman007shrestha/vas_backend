4004c7f482ec953470eb894a5594fd423c87cb8f
aman007shrestha <aman13xrestha@gmail.com>
2022-08-03 22:43:41 +0545

    starter template

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,1 @@
-import express, { Application } from "express";
-import dotenv from "dotenv";
 
-dotenv.config();
-const app: Application = express();
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`App initialized at port ${PORT}`);
-});
